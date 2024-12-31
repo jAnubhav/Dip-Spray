94607d3a239fae1ba5dbda49f7212eaae0a79149
Anubhav Jain <jainanubhav2003@gmail.com>
2024-12-31 21:14:54 +0530

    frontend done

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import './App.css';
 
 const App: React.FC = () => {
 	return (
-		<section className='d-flex fld-col jc-center container'>
+		<section className='d-flex jc-center container'>
 			<div className='d-grid main-grid'>
 				<div className='d-flex jc-center logo'> <img src='./img/logo.svg' alt='Logo' /> </div>
 				<div className='d-flex jc-center'> <p className='title'>Dip Spray</p> </div>
 
-				<div className='d-flex'>
-					<p className='tagline'>Just a twist of the tap, and 1 Aptos DevNet token flows your way - fueling your blockchain innovations!</p>
-				</div>
+				<div className='d-flex'> <p className='tagline'>
+					Just a twist of the tap, and 1 Aptos DevNet token flows your way - fueling your blockchain innovations!
+				</p> </div>
 
-				<div> <input type='text' name='account' id='account' placeholder='Account Address' /> </div>
-				<div> <input type='number' name='quan' id='quan' value={1} min={1} /> </div>
-				<div> <input type='button' value='Fuel It' /> </div>
+				<div> <input className='inp acc' type='text' id='account' placeholder='Account Address' /> </div>
+				<div> <input className='inp quan' type='number' id='quan' defaultValue={1} min={1} max={10} /> </div>
+				<div> <input className='inp btn' type='button' value='Send' /> </div>
 			</div>
 		</section>
 	)
