eb8d9e0e4acbdcffa050381921bbef1af40277a7
Anubhav Jain <jainanubhav2003@gmail.com>
2025-01-01 17:00:02 +0530

    funder done

--- src/assets/funder.tsx
+++ src/assets/funder.tsx
@@ -2,11 +2,19 @@ import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 
 const aptos: Aptos = new Aptos(new AptosConfig({ network: Network.DEVNET }));
 
-const funder = async () => {
-    await aptos.fundAccount({ 
-        accountAddress: "0xcbfce9416602fbe8ebf8c748c472a47e5a2d46174b7ab979f491d2377fca9491", 
-        amount: 100_000_000 
-    });
-};
+const fund_account = async () => {
+    const acc: string = (document.getElementById("acc") as HTMLInputElement).value;
+    const quan: string = (document.getElementById("quan") as HTMLInputElement).value;
 
-export default funder;
+    if (acc == '' || quan == '') return;
+
+    const btn: HTMLInputElement = document.getElementById("btn") as HTMLInputElement;
+    btn.disabled = true; btn.classList.add("disabled");
+
+    for (let i = 0; i < parseInt(quan); i++) {
+        await aptos.fundAccount({ accountAddress: acc, amount: 100_000_000 }); }
+
+    btn.disabled = false; btn.classList.remove("disabled")
+}
+
+export default fund_account;
