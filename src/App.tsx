04723a94ddfd2e5b95dff725f6b81ac70127b9a8
Anubhav Jain <jainanubhav2003@gmail.com>
2025-01-02 17:28:13 +0530

    new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
+// import { check_valid, fund_account } from './assets/funder';
+
 import './App.css';
 
 const App: React.FC = () => {
 	return (
 		<section className='d-flex jc-center container'>
-			<div className='d-grid main-grid'>
+			{/* <div className='d-grid main-grid'>
 				<div className='d-flex jc-center logo'> <img src='./img/logo.svg' alt='Logo' /> </div>
 				<div className='d-flex jc-center'> <p className='title'>Dip Spray</p> </div>
 
 				<div className='d-flex'> <p className='tagline'>
-					Just a twist of the tap, and 1 Aptos DevNet token flows your way - fueling your blockchain innovations!
+					Just a twist of the tap, and 1Apt DevNet token flows your way - fueling your blockchain innovations!
 				</p> </div>
 
-				<div> <input className='inp acc' type='text' id='account' placeholder='Account Address' /> </div>
-				<div> <input className='inp quan' type='number' id='quan' defaultValue={1} min={1} max={10} /> </div>
-				<div> <input className='inp btn' type='button' value='Send' /> </div>
-			</div>
+				<div> <input type='text' className='inp' id='acc' placeholder='Add Account Address' /> </div>
+				<div> <input type='number' className='inp' id='quan' defaultValue={1} min={1} max={10} /> </div>
+				<div> <input type='button' className='inp dis' id='btn' value='Send' /> </div>
+			</div> */}
 		</section>
 	)
 };
