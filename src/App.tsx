e86b13574b7c89659d3f8638aef627e8e7043a21
Anubhav Jain <jainanubhav2003@gmail.com>
2025-01-05 13:36:29 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { Variables } from "./assets/funder";
+
 import "./css/App.css";
 import "./css/Resp.css";
 
 const App: React.FC = () => {
+	const { update_acc, update_quan, fund_account, acc_input, quan_input, btn_state } = Variables();
+
 	return (
 		<section className="d-grid" id="container">
 			<section className="d-flex jc-center" id="main-container">
@@ -11,13 +15,24 @@ const App: React.FC = () => {
 					<div className="long-sec" id="logo-image"> <img src="img/logo.svg" alt="Logo" /> </div>
 					<div className="d-flex jc-center long-sec"> <p id="logo-name">Dip Spray</p> </div>
 
-					<div className="d-flex jc-center long-sec"> <p id="tagline"> 
-						Just a twist of the tap, and 1 Aptos DevNet token flows your way—fueling your blockchain innovations one drop at a time! 
+					<div className="d-flex jc-center long-sec"> <p id="tagline">
+						Just a twist of the tap, and 1 Aptos DevNet token flows your way - fueling your blockchain innovations one drop at a time!
 					</p> </div>
 
-					<div className="d-flex" id="acc-con"> <input type="text" className="inp" id="acc" placeholder="Account Address" /> </div>
-					<div className="d-flex"> <input type="number" className="inp" id="quan" min={1} defaultValue={1} max={10} /> </div>
-					<div className="d-flex"> <input type="button" className="inp" id="btn" value="Fuel It" /> </div>
+					<div className="d-flex" id="acc-con">
+						<input type="text" className="inp" id="acc" placeholder="Account Address"
+							onChange={update_acc} value={acc_input} />
+					</div>
+
+					<div className="d-flex">
+						<input type="number" className="inp" id="quan" min={1} max={10}
+							onChange={update_quan} value={quan_input} />
+					</div>
+
+					<div className="d-flex">
+						<input type="button" className={`inp ${btn_state}`} id="btn"
+							onClick={fund_account} value="Fuel It" />
+					</div>
 				</div>
 			</section>
 
---
+++ src/assets/funder.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+
+const aptos: Aptos = new Aptos(new AptosConfig({ network: Network.DEVNET }));
+
+const isHex = (val: string): boolean => /^(0x|0X)?[a-fA-F0-9]+$/.test(val);
+
+export const Variables = () => {
+    const [ acc_input, set_acc ] = useState<string>("");
+    const [ quan_input, set_quan ] = useState<number>(1);
+    const [ btn_state, set_btn ] = useState<string>("disabled");
+
+    const update_acc = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        let val: string = e.target.value; set_acc(val);
+
+        if (val.startsWith("0x") && val.length == 66 && isHex(val)) {
+            set_btn("active"); } else set_btn("disabled");
+    };
+    
+    const update_quan = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        let val: number = parseInt(e.target.value); if (val != 10 && val % 10 == 0) val /= 10; 
+        else if (val > 10) val %= 10; else if (isNaN(val)) val = 1; set_quan(val);
+    };
+
+    
+    const fund_account = async () => {
+        for (let i = 0; i < quan_input; i++) await aptos.fundAccount(
+            { accountAddress: acc_input, amount: 100_000_000 });
+    };
+
+    return { update_acc, update_quan, fund_account, acc_input, quan_input, btn_state };
+};
